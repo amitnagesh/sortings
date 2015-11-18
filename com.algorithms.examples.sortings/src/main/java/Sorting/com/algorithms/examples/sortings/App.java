b66d29dcdeac7db3a6ca3cdea860bffff6b11e64
anagesh <amitnageshtests@gmail.com>
2015-11-18 15:43:30 -0800

    Palindrome change: Updating terminating condition.

--- com.algorithms.examples.sortings/src/main/java/Sorting/com/algorithms/examples/sortings/App.java
+++ com.algorithms.examples.sortings/src/main/java/Sorting/com/algorithms/examples/sortings/App.java
@@ -8,12 +8,12 @@ public class App  extends QuickSort {
 
 	public static void main( String[] args ) {
 
-//			int[] in = {3,2,6,1,7,5,8,10,110,200,41,29};
+			int[] in = {3,2,6,1,7,5,8,10,110,200,41,29};
 		//	int[] in = {-1, 3, 5, (int) 2.0, 8 };
 		
 
-		//	BubbleSort bs = new BubbleSort();
-		//	bs.bubbleSort(in);
+			BubbleSort bs = new BubbleSort();
+			bs.bubbleSort(in);
 		
 		//	SelectionSort ss = new SelectionSort();
 		//	ss.selectionSort(in);
---
+++ com.algorithms.examples.sortings/src/main/java/Sorting/com/algorithms/examples/sortings/Palindrome.java
@@ -0,0 +1,30 @@
+package Sorting.com.algorithms.examples.sortings;
+
+public class Palindrome {
+	
+	public static void main(String args[]){
+		
+		String input[]  = {"abccba1", "a" , "abc" , "aabcc", "aaa" , "abcdeffedcba", "aaaaaaaaaaabbaaaaaaaaaaa", null, "", "baabaa"};
+		
+		for(String str: input){
+			System.out.println( str + " is palindrome :" + isPalindrome(str));
+		}
+	}
+	
+	public static boolean isPalindrome(String in) {
+
+		if(in==null || in.length()==0 || in.length()==1)
+			return true;
+		
+		int beginPtr =0, endPtr = in.length()-1;
+		
+		while(beginPtr<=endPtr && (in.charAt(beginPtr++) == in.charAt(endPtr--))) ;
+		
+		if(in.charAt(beginPtr) == in.charAt(endPtr) && beginPtr>=endPtr)
+			return true;
+		
+		return false;
+
+	}
+
+}
